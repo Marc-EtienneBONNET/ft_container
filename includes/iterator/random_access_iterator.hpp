2e5fec4748846e1ceb0ae7d06a4576f327d4d128
Marc-etienne BONNET <mbonnet@e2r8p13.clusters.42paris.fr>
2022-04-12 18:52:12 +0200

    correction et ajoue au main de test des iterator

--- includes/iterator/random_access_iterator.hpp
+++ includes/iterator/random_access_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: mbonnet <mbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 16:35:14 by mbonnet           #+#    #+#             */
-/*   Updated: 2022/04/07 17:58:15 by mbonnet          ###   ########.fr       */
+/*   Updated: 2022/04/12 18:51:30 by mbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 template<typename T>
 class R_A_iterator : std::iterator<std::random_access_iterator_tag, T>
-{
+{ 
 public :
     R_A_iterator(void)                                  {};
     R_A_iterator(R_A_iterator &ori)                     {(void)ori;};
@@ -32,22 +32,22 @@ public :
     //void         operator--(R_A_iterator &ori)          {ori.getPointeur()--;};
     void         operator++(void)                       {this->pointeur++;};
     void         operator--(void)                       {this->pointeur--;};
-    T            operator+(R_A_iterator &ori) const     {return (*(this->pointeur) + ori.getValPointeur());};
-    T            operator-(R_A_iterator &ori) const     {return (*(this->pointeur) - ori.getValPointeur());};;
-    T            operator+(T val) const                 {return (*(this->pointeur) + val);};
-    T            operator-(T val) const                 {return (*(this->pointeur) - val);};
+    T            *operator+(T val) const                 {return (&(this->pointeur[val]));};
+    T            *operator-(T val) const                 {T *tmp; tmp = pointeur; while (val-- > 0){tmp--;} return (tmp);};
+    //T            *operator+(T val) const                 {return (&(this->pointeur[val]));};
+    //T            *operator-(T val) const                 {T *tmp; tmp = pointeur; while (val-- > 0){tmp--;} return (tmp);};
     void         operator+=(R_A_iterator &ori)          {*(this->pointeur) += ori.getValPointeur();};
     void         operator-=(R_A_iterator &ori)          {*(this->pointeur) -= ori.getValPointeur();};
     void         operator+=(T val)                      {*(this->pointeur) += val;};
     void         operator-=(T val)                      {*(this->pointeur) -= val;};
-    int          operator==(R_A_iterator &ori) const    {if (this->pointeur == ori.getPointeur()) return (0); else return (1);};
-    int          operator==(T val) const                {if (*(this->pointeur) == val) return (0); else return (1);};
-    int          operator!=(R_A_iterator &ori) const    {if (this->pointeur != ori.getPointeur()) return (0); else return (1);};
-    int          operator!=(T val) const                {if (*(this->pointeur) != val) return (0); else return (1);};
-    int          operator<=(R_A_iterator &ori) const    {if (*(this->pointeur) <= ori.getValPointeur()) return (0); else return (1);};
-    int          operator<(R_A_iterator &ori) const     {if (*(this->pointeur) < ori.getValPointeur()) return (0); else return (1);};
-    int          operator>=(R_A_iterator &ori) const    {if (*(this->pointeur) >= ori.getValPointeur()) return (0); else return (1);};
-    int          operator>(R_A_iterator &ori) const     {if (*(this->pointeur) > ori.getValPointeur()) return (0); else return (1);};
+    bool          operator==(R_A_iterator &ori) const    {if (this->pointeur == ori.getPointeur()) return (true); else return (false);};
+    bool          operator==(T val) const                {if (*(this->pointeur) == val) return (true); else return (false);};
+    bool          operator!=(R_A_iterator &ori) const    {if (this->pointeur != ori.getPointeur()) return (true); else return (false);};
+    bool          operator!=(T val) const                {if (*(this->pointeur) != val) return (true); else return (1);};
+    bool          operator<=(R_A_iterator &ori) const    {if (*(this->pointeur) <= ori.getValPointeur()) return (true); else return (false);};
+    bool          operator<(R_A_iterator &ori) const     {if (*(this->pointeur) < ori.getValPointeur()) return (true); else return (false);};
+    bool          operator>=(R_A_iterator &ori) const    {if (*(this->pointeur) >= ori.getValPointeur()) return (true); else return (false);};
+    bool          operator>(R_A_iterator &ori) const     {if (*(this->pointeur) > ori.getValPointeur()) return (true); else return (false);};
     T            operator[](int index)                  {return (this->pointeur[index]);};
     T            *operator<<(R_A_iterator &ori)         {return (ori->getPointeur());};
 protected :
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbonnet <mbonnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 13:09:38 by mbonnet           #+#    #+#             */
-/*   Updated: 2022/04/07 18:02:37 by mbonnet          ###   ########.fr       */
+/*   Updated: 2022/04/12 18:48:57 by mbonnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,41 @@
 
 int	main(void)
 {
-	int	tab[] = {1,2,3,4};
+	int	tab[] = {1,20,30,40};
 	R_A_iterator<int> test_iterator(tab);
+	R_A_iterator<int> tmp;
+	R_A_iterator<int> tmp2;
+	std::cout << VIOLET << "nombre rentrer dans le test : 1, 20, 30, 40" << BLANC << std::endl; 
 	std::cout << VIOLET << "--------Random_access_iterator--------" << BLANC << std::endl;
 	std::cout << "methode getVal : " << test_iterator.getValPointeur() << std::endl;
 	std::cout << "methode get    : " << test_iterator.getPointeur() << std::endl;
-	std::cout << "---operateur++ ---" << std::endl;
+	std::cout << BLEU << "---operateur(++) ---" << BLANC << std::endl;
 	++test_iterator;
 	std::cout << "methode getVal : " << test_iterator.getValPointeur() << std::endl;
-	std::cout << "---operateur-- ---" << std::endl;
+	std::cout << BLEU << "---operateur(--) ---" << BLANC << std::endl;
 	--test_iterator;
 	std::cout << "methode getVal : " << test_iterator.getValPointeur() << std::endl;
+	std::cout << BLEU << "---operateur(+(+2)) ---" << BLANC << std::endl;
+	tmp = test_iterator + 2;
+	std::cout << "methode getVal : " <<  tmp.getValPointeur() << std::endl;
+	std::cout << BLEU << "---operateur(-(-2)) ---" << BLANC << std::endl;
+	tmp2 = tmp - 4;
+	std::cout << "methode getVal : " << tmp2.getValPointeur() << std::endl;
+	std::cout << BLEU << "---operateur(+=3) ---" << BLANC << std::endl;
+	test_iterator += 3;
+	std::cout << "methode getVal : " << test_iterator.getValPointeur() << std::endl;
+	std::cout << BLEU << "---operateur(-=3) ---" << BLANC << std::endl;
+	test_iterator -= 3;
+	std::cout << "methode getVal : " << test_iterator.getValPointeur() << std::endl;
+	std::cout << BLEU << "---operateur(+= lui meme) ---" << BLANC << std::endl;
+	test_iterator += test_iterator.getValPointeur();
+	std::cout << "methode getVal : " << test_iterator.getValPointeur() << std::endl;
+	std::cout << BLEU << "---operateur(-= lui meme) ---" << BLANC << std::endl;
+	test_iterator -= test_iterator.getValPointeur();
+	std::cout << "methode getVal : " << test_iterator.getValPointeur() << std::endl;
+
+
 
 	//std::cout << "operator << : " << test_iterator << std::endl;
-	
-	
-
 	return (0);
 }
